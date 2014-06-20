1055f3abe0fbe987769646af00e4cf1763b7ee10
takumi <everybody.say.hey.yo@gmail.com>
2014-06-21 01:21:07 +0900

    AutoLoginを編集

--- AutoLogin/background.js
+++ AutoLogin/background.js
@@ -1,22 +1,45 @@
 
-alert(tab.url.indexOf('e'));
 
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
-	if(tab.url.indexOf('dotinstall') != -1){
-		chrome.PageAction.show(tabId);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	if(tab.url.indexOf('Portallogin') != -1){
+		chrome.pageAction.show(tabId);
 	}
 	});
 
-chrome.pageAction.onClicked.addListener(function(){
-	chrome.tabs.executeScript(null, {
-		"code":"document.body.style.backgroundColor='red'"
-	})
+chrome.pageAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript(null,
+		{file: "myscript.js"});
 });
 
-// chrome.browserAction.onClicked.addListener(function(){
-// 	var color = localStorage['color'] ? localStorage['color'] :'red';
-// 	chrome.tabs.executeScript(null,{
-// 	"code":"document.body.style.backgroundColor='"+color+"'"
+// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+// 	if(tab.url.indexOf('dotinstall') != -1){
+// 		chrome.PageAction.show(tabId);
+// 	}
 // 	});
+//chrome.pageAction.show(integer tabId);
+// chrome.pageAction.onClicked.addListener(function(){
+// 	chrome.tabs.executeScript(null, {
+// 		"code":"change()"
+// 	})
 // });
 
+
+
+// function change(){
+// 	var a = document.getElementsByTagName("th");
+//   console.log(a[6].innerHTML);
+//   console.log(a[8].innerHTML);
+//   console.log(a[10].innerHTML);
+
+//   console.log("["+(a[6].innerHTML.substr(1,1).charCodeAt(0)-65)+","+(a[6].innerHTML.substr(3,1)-1)+"]=["+"]");
+
+// //テキストボックスに文字を入力
+//   var b = document.getElementsByName("message3"),
+//       c = document.getElementsByName("message4"),
+//       d = document.getElementsByName("message5");
+  
+//   b[0].value = "abc";
+//   c[0].value = "abc";
+//   d[0].value = "abc";
+
+// }
--- AutoLogin/myscript.js
+++ AutoLogin/myscript.js
@@ -1,6 +1,30 @@
-var x=0,y=0;
-			document.getElementById('save').onclick = function(){
-				//alert("This name is "+x+" and "+y);
-				document.getElementById('text1').innerHTML="Stay";
-			}
-		
+
+// chrome.extension.sendRequest({"action": "getName"}, function(response) {
+//                            var name = response;
+//                            if (name == '' || name == null) {
+//                                name = 'guest';
+//                            }
+//                            var hello = document.createElement('div');
+//                            hello.innerText = "Hello, " + name;
+//                            var d = document.getElementsByTagName('body')[0];
+//                            var dp = d.parentNode;
+//                            dp.insertBefore(hello, d);
+//                        });
+
+
+
+var a = document.getElementsByTagName("th");
+  console.log(a[6].innerHTML);
+  console.log(a[8].innerHTML);
+  console.log(a[10].innerHTML);
+
+  console.log("["+(a[6].innerHTML.substr(1,1).charCodeAt(0)-65)+","+(a[6].innerHTML.substr(3,1)-1)+"]=["+"]");
+
+//テキストボックスに文字を入力
+  var b = document.getElementsByName("message3"),
+      c = document.getElementsByName("message4"),
+      d = document.getElementsByName("message5");
+  
+  b[0].value = "abc";
+  c[0].value = "abc";
+  d[0].value = "abc";
