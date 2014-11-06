bd0452d38e041f6305d7098915d8d49605dc0912
takumi shimojo <takumin@takumi.(none)>
2014-11-06 22:11:14 +0900

    削除

--- BrowserAction3(今いるページの背景色を変える)/background.js
+++
@@ -1,7 +0,0 @@
-
-chrome.browserAction.onClicked.addListener(function(){
-	chrome.tabs.executeScript(null,{
-	"code":"document.body.style.backgroundColor='green'"
-	});
-});
-
--- yamasita/input.js
+++
@@ -1,3 +0,0 @@
-document.getElementsByName("message3")[0].value="huga";
-document.getElementsByName("message4")[0].value="huga";
-document.getElementsByName("message5")[0].value="foo";
